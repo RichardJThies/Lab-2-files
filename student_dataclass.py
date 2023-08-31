2f8ee3aad317b0ec9cb093acb51545fd5b581730
SalvLav <richardjthies95@gmail.com>
2023-08-30 22:52:01 -0500

    Finished part 3

--- student_dataclass.py
+++ student_dataclass.py
@@ -9,16 +9,23 @@ class Student:#function inside a class is a method, outside methods are just fun
     gpa: float
 
 def main():#would you use a dictonary to hold new students if you were adding them?
-    newStudentInfo = Student(newStudentName, newStudentID, newStudentGPA)
-    
-    print('Please enter information about new student. Hit enter to quit.')
-    newStudentName = input('New name? ')
-    newStudentID = input('New ID? ')
-    newStudentGPA = input('New GPA? ')
-    while True:
-        if newStudentName or newStudentID or newStudentGPA == '':
-            break
-    print(newStudentInfo)
+    tim = Student('tim', '12345', '3.1')#P
+    print(tim.name)
+    print(tim.schoolID)
+    print(tim.gpa)
+    print(tim)#Python has still generated a __init__ and __str__ method in the background
+
+    sarah = Student('sarah', '25642yr', '3.3')
+    print(sarah.name)
+    print(sarah.schoolID)
+    print(sarah.gpa)
+    print(sarah)#to customize this generated __init__ and __str__ methods, look into the python documentation
+
+    jake = Student('jake', 'dgj5463', '3.0')
+    print(jake.name)
+    print(jake.schoolID)
+    print(jake.gpa)
+    print(jake)
 
 if __name__ == '__main__':#this construction is for when files are importing code from other files, all could will run by default, this stops that from occuring
     main()
